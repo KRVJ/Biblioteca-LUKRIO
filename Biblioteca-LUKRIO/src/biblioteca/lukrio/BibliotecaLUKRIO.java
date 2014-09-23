cdd6e8f4182789b1584efa7acc6d7b7273e20a25
Usuario <Usuario@192.168.0.6>
2014-09-22 21:05:05 -0600

    Kevin :3

--- Biblioteca-LUKRIO/src/biblioteca/lukrio/BibliotecaLUKRIO.java
+++ Biblioteca-LUKRIO/src/biblioteca/lukrio/BibliotecaLUKRIO.java
@@ -1,6 +1,7 @@
 package biblioteca.lukrio;
 public class BibliotecaLUKRIO {
     public static void main(String[] args) {
+        BibliotecaLUKRIO biblioteca=new BibliotecaLUKRIO();
         System.out.println("Prubea hola");
     }
     
